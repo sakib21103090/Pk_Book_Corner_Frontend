750c425c9d270cdbade6ab2e72fbaef95d981605
sakib21103090 <21103090@iubat.edu>
2024-07-11 12:35:47 +0600

    check out page design

--- src/features/Cart/Cart.js
+++ src/features/Cart/Cart.js
@@ -121,13 +121,13 @@ export default function Cart() {
               </p>
               <div className="flex space-x-4">
                 <button
-                  onClick={() => handleDeliveryCharge(120)}
+                  onClick={() => handleDeliveryCharge(90)}
                   className="px-4 py-2 bg-yellow-500 text-white rounded-full shadow-lg hover:bg-yellow-600 focus:outline-none transition duration-300"
                 >
                   Dhaka
                 </button>
                 <button
-                  onClick={() => handleDeliveryCharge(150)}
+                  onClick={() => handleDeliveryCharge(120)}
                   className="px-4 py-2 bg-green-500 text-white rounded-full shadow-lg hover:bg-green-600 focus:outline-none transition duration-300"
                 >
                   Without Dhaka
